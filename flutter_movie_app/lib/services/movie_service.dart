e833fde282fb53cd1671a679443a03046380d3c4
Jose Developer <jeduramos7001@gmail.com>
2026-04-25 05:41:09 -0600

    add: Flutter base app

---
+++ flutter_movie_app/lib/services/movie_service.dart
@@ -0,0 +1,15 @@
+// Packages
+import 'package:get_it/get_it.dart';
+
+// Services
+import '../services/http_service.dart';
+
+class MovieService {
+  final GetIt getIt = GetIt.instance;
+
+  HTTPService _http;
+
+  MovieService() {
+    _http = getIt.get<HTTPService>();
+  }
+}
